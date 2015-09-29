58111c65dda7809aa72f699ca4637670464a153e
Andregdm <andreg.dmenezes@gmail.com>
2015-09-29 10:58:21 -0300

    Ajustando
    Acesso ao Banco de Dados do Coltec

--- connection.php
+++ connection.php
@@ -2,10 +2,10 @@
 
 class Connection
 {
-  public static $database = "serasa";
-  public static $address = "127.0.0.1";
-  public static $user = "root";
-  public static $password = "";
+  public static $database = "daw-aluno4-cms";
+  public static $address = "150.164.102.160";
+  public static $user = "daw-aluno4";
+  public static $password = "andre";
 
   public static function getConnection() {
     $connection = mysqli_connect(Connection::$address, Connection::$user, Connection::$password, Connection::$database);
