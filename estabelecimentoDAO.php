bc8f4a4c90fb93b1417b3d6159e2b978b16f1b4d
Andregdm <andreg.dmenezes@gmail.com>
2015-10-01 10:09:04 -0300

    Criação e ajustes
    Acréscimo da classe EstabelecimentoDAO nos moldes da classe ClienteDAO

---
+++ estabelecimentoDAO.php
@@ -0,0 +1,71 @@
+<?php
+
+require 'connection.php';
+
+class EstabelecimentoDAO
+{
+
+  public static function getEstabelecimentoByNome($nome) {
+    $connection = Connection::getConnection();
+    $sql = "SELECT * FROM estabelecimentos WHERE nome=$nome";
+    $result  = mysqli_query($connection, $sql);
+    $estabelecimento = mysqli_fetch_object($result);
+
+    //recupera cidade do estabelecimento
+    $sql = "SELECT * FROM cidades WHERE id=$estabelecimento->cidades_id";
+    $result = mysqli_query($connection, $sql);
+    $estabelecimento->cidade =  mysqli_fetch_object($result);
+
+    return $estabelecimento;
+  }
+
+
+  public static function getAll()
+  {
+    $connection = Connection::getConnection();
+    $sql = "SELECT * FROM estabelecimentos";
+
+    // recupera todos os clientes
+    $result  = mysqli_query($connection, $sql);
+    $estabelecimentos = array();
+    while ($estabelecimento = mysqli_fetch_object($result)) {
+      if ($estabelecimento != null) {
+        $estabelecimentos[] = $estabelecimento;
+      }
+    }
+    return $estabelecimentos;
+  }
+
+
+  public static function updateEstabelecimento($estabelecimento, $id) {
+    $connection = Connection::getConnection();
+    $sql = "UPDATE estabelecimentos SET nome='$estabelecimento->nome', cidades_id=$estabelecimento->cidades_id WHERE id=$id";
+    $result  = mysqli_query($connection, $sql);
+
+    $estabelecimentoAtualizado = ClienteDAO::getEstabelecimentoByNome($estabelecimento->nome);
+    return $estabelecimentoAtualizado;
+  }
+
+
+  public static function deleteEstabelecimento($id) {
+    $connection = Connection::getConnection();
+    $sql = "DELETE FROM estabelecimentos WHERE id=$id";
+    $result  = mysqli_query($connection, $sql);
+
+    if ($result === FALSE) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+  public static function addEstabelecimento($estabelecimento) {
+    $connection = Connection::getConnection();
+    $sql = "INSERT INTO estabelecimentos (nome, cidades_id) VALUES ('$estabelecimento->nome', $estabelecimento->cidades_id)";
+    $result  = mysqli_query($connection, $sql);
+
+    $novoEstabelecimento = ClienteDAO::getClienteByCPF($estabelecimento->cpf);
+    return $novoEstabelecimento;
+  }
+}
